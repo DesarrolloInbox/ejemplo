79d3ff1a0966e440b535c9375f360a23446c4b77
Daniel <ad.gb@hotmail.com>
2026-01-25 10:50:10 -0600

    DotTech Comunicación entre componentes - Templates 5

--- Frente/src/app/componentes/item-heroe/articulo-heroe.ts
+++ Frente/src/app/componentes/item-heroe/articulo-heroe.ts
@@ -1,10 +1,11 @@
 import { Component, computed, input, output } from '@angular/core';
 import { Heroe, Poder } from '../../compartido/interfaces/heroe.interface';
 import { HeroeCambioPoderes } from '../../compartido/interfaces/heroe-cambio-poderes.interface';
+import { KeyValuePipe, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-articulo-heroe',
-  imports: [],
+  imports: [KeyValuePipe, TitleCasePipe],
   templateUrl: './articulo-heroe.html',
   styleUrl: './articulo-heroe.css',
 })
